5446aa6c49b152ff568bfc224f35408dfb3aa938
A. Theo <adryantheo@gmail.com>
2021-05-24 10:23:09 +0700

    Input Input

--- src/Components/Login/view/NameInput.jsx
+++ src/Components/Login/view/NameInput.jsx
@@ -1,10 +1,17 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 
 export default function NameInput(props){
-    const [nama, setNama] = useState("cek");
+    const [nama, setNama] = useState("");
+
+    useEffect(() => {
+        setNama("Hello");
+    },[]) 
 
 
+    const onTodo = (val)=> {
+        setNama(val)
+    }
 
     // ini buat ganti nama
     // const cobaGanti = async () => {
@@ -13,7 +20,7 @@ export default function NameInput(props){
 
     return(
         <div style={{paddingTop: '20px', paddingBottom: '20px'}}>
-            <input id="nama" type="text" placeholder="Masukkan Nama"  />
+            <input id="nama" type="text" placeholder="Masukkan Nama" value={nama} onChange={e => onTodo(e.target.value)} />
             <button onClick={async() => setNama('')}>Hapus Nama</button>
         </div>
     )
